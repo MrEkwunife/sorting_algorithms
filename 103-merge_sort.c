f5457e9fcb3bd0a5c2aee45b77c9239a3b63c569
Kenechukwu Akubue <kengoon19@gmail.com>
2024-02-21 05:56:44 +0100

    103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -2,39 +2,39 @@
 
 /**
  * merge_subarray - Sort a subarray of integers.
- * @subarr: A subarray of an array of integers to sort.
+ * @subarray: A subarray of an array of integers to sort.
  * @buff: A buffer to store the sorted subarray.
  * @front: The front index of the array.
  * @mid: The middle index of the array.
  * @back: The back index of the array.
  */
-void merge_subarray(int *subarr, int *buff, size_t front, size_t mid,
+void merge_subarray(int *subarray, int *buff, size_t front, size_t mid,
 		size_t back)
 {
 	size_t i, j, k = 0;
 
 	printf("Merging...\n[left]: ");
-	print_array(subarr + front, mid - front);
+	print_array(subarray + front, mid - front);
 
 	printf("[right]: ");
-	print_array(subarr + mid, back - mid);
+	print_array(subarray + mid, back - mid);
 
 	for (i = front, j = mid; i < mid && j < back; k++)
-		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+		buff[k] = (subarray[i] < subarray[j]) ? subarray[i++] : subarray[j++];
 	for (; i < mid; i++)
-		buff[k++] = subarr[i];
+		buff[k++] = subarray[i];
 	for (; j < back; j++)
-		buff[k++] = subarr[j];
+		buff[k++] = subarray[j];
 	for (i = front, k = 0; i < back; i++)
-		subarr[i] = buff[k++];
+		subarray[i] = buff[k++];
 
 	printf("[Done]: ");
-	print_array(subarr + front, back - front);
+	print_array(subarray + front, back - front);
 }
 
 /**
  * merge_sort_recursive - Implement the merge sort algorithm through recursion.
- * @subarr: A subarray of an array of integers to sort.
+ * @subarray: A subarray of an array of integers to sort.
  * @buff: A buffer to store the sorted result.
  * @front: The front index of the subarray.
  * @back: The back index of the subarray.
